e9685c7e546103a8328dfddb13a5cfde36d76948
Leslie Hsu <chuckleberries@hotmail.com>
2017-10-17 15:58:19 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
   new_array = Array.new
   array.each do |base|
-    square = base**2
-    new_array << square
-    array.shift
+    new_array << base**2
+    end
   end  # your code here
-  array = new_array
 end
