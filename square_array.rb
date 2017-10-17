ddd6040acdc844f25f3b6d2101a3d7753e7df255
Leslie Hsu <chuckleberries@hotmail.com>
2017-10-17 16:02:15 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,15 @@
+=begin 
 def square_array(array)
   new_array = Array.new
   array.each do |base|
     new_array << base**2
   end
   return new_array
+end
+=end
+
+def square_array(array)
+  array.collect do |base|
+    base**2
+  end
 end
