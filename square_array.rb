2b536db7d4340eb1256ff33e021734820788ad53
Leslie Hsu <chuckleberries@hotmail.com>
2017-10-17 16:14:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+
 def square_array(array)
-  # your code here
+  new_array = Array.new
+  array.each do |base|
+    new_array << base**2
+  end
+  return new_array
 end
+
+=begin
+def square_array(array)
+  new_array = array.collect do
+    |base| base**2
+  end
+  return new_array
+end
+=end
